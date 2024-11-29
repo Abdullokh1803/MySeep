48970743fb93951100c471f34b4995d2b879ad45
Abdullokh1803 <abdullokhm2005@gmail.com>
2024-11-29 14:19:45 +0600

    copy

--- src/components/titles/subtitle.tsx
+++ src/components/titles/subtitle.tsx
@@ -1,6 +1,6 @@
 import classes from '~/src/components/titles/titles.module.scss'
 
-export default function Subtitle({children}){
+export default function Subtitle({children}:{ children: React.ReactNode }){
     return(
         <h1 className={classes.subtitlee}>{children}</h1>
     )
--- src/components/titles/title.tsx
+++ src/components/titles/title.tsx
@@ -1,6 +1,6 @@
 import classes from '~/src/components/titles/titles.module.scss'
 
-export default function Title({children}){
+export default function Title({children}: { children: React.ReactNode }){
     return(
         <h1 className={classes.title}>{children}</h1>
     )
