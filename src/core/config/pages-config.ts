ce6326b22fec89df9385df93e2f7d46959c4797b
Abdullokh1803 <abdullokhm2005@gmail.com>
2024-11-29 15:27:33 +0600

    copy

--- src/core/config/pages-config.ts
+++ src/core/config/pages-config.ts
@@ -1,14 +1,12 @@
-
 class PagesConfig {
   home = '/';
   about = '/about';
   news = '/news';
   masters = '/masters';
+  video ='/video';
   services = '/services';
   partners = '/partners';
   contacts = '/contacts';
-  videoLessons ='/videoLessons'
-
   // Method to get paths with locale
   getPath(route: keyof PagesConfig, locale: string) {
     // Adding locale in the path (e.g., /en/about)
